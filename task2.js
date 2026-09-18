bf2da1afe1973ac902e7eb845033f4e01dbc459b
Polina Borovaya <polina.20000529@mail.ru>
2026-09-18 17:43:44 +0300

    Add files via upload

---
+++ task2.js
@@ -0,0 +1,10 @@
+const table = document.querySelector('table');
+
+table.addEventListener('click', e => {
+    const cell = e.target.closest('td');
+
+    if(!cell) return;
+
+    const row = cell.closest('tr');
+    row.classList.add('selected');
+});
