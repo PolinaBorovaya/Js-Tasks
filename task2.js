170585c217b545403ce036600fa613a9752c7aee
Polina Borovaya <polina.20000529@mail.ru>
2026-09-21 15:43:09 +0300

    Задание 7 (dom)
    Убраны магические числа

--- task2.js
+++ task2.js
@@ -3,7 +3,9 @@ const table = document.querySelector('table');
 table.addEventListener('click', e => {
     const cell = e.target.closest('td');
 
-    if(!cell) return;
+    if(!cell) {
+        return;
+    }
 
     const row = cell.closest('tr');
     row.classList.add('selected');
